ab98ae9198c9f065339d3324dd44abbf16bf3e9e
abu24dev <muhammedahmed2402@gmail.com>
2025-12-05 14:23:04 +0200

    Breathing

--- src/FinalP/AnimGLEventListener3.java
+++ src/FinalP/AnimGLEventListener3.java
@@ -1,183 +1,220 @@
 package FinalP;
+
 import FinalP.Texture.TextureReader;
+
 import java.awt.event.*;
 import java.io.IOException;
 import javax.media.opengl.*;
-import java.util.BitSet;
 import javax.media.opengl.glu.GLU;
+import java.util.BitSet;
 
 public class AnimGLEventListener3 extends AnimListener {
 
-    int animationIndex = 0;
-    int maxWidth = 100;
-    int maxHeight = 100;
+    int maxWidth = 100, maxHeight = 100;
     int x = 20, y = 20;
-    
-    // Download enemy textures from https://craftpix.net/freebies/free-monster-2d-game-items/
-    String textureNames[] = {"Walk1.png","Walk2.png","Walk3.png","Walk4.png","Walk5.png","Walk6.png","Walk7.png","Walk8.png","7.png"};
-    TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
-    int textures[] = new int[textureNames.length];
 
-    /*
-     5 means gun in array pos
-     x and y coordinate for gun 
-     */
+    // -------------------------
+    // TEXTURES (Option A)
+    // -------------------------
+    // 8 walking frames + 6 idle frames + 1 background
+    String textureNames[] = {
+            // WALK FRAMES (0–7)
+            "Walk1.png", "Walk2.png", "Walk3.png", "Walk4.png",
+            "Walk5.png", "Walk6.png", "Walk7.png", "Walk8.png",
+
+            // IDLE FRAMES (8–13)
+            "Idle1.png", "Idle2.png", "Idle3.png",
+            "Idle4.png", "Idle5.png", "Idle6.png",
+
+            // BACKGROUND (14)
+            "7.png"
+    };
+
+    TextureReader.Texture[] texture = new TextureReader.Texture[textureNames.length];
+    int[] textures = new int[textureNames.length];
+
+    // ------- Animation Settings -------
+    int walkIndex = 0;
+    int idleIndex = 0;
+    int walkFrames = 8;
+    int idleFrames = 6;
+
+    int frameDelay = 0;
+    boolean facingLeft = false;
+
     public void init(GLAutoDrawable gld) {
 
         GL gl = gld.getGL();
-        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);    //This Will Clear The Background Color To Black
-        
-        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
-        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);	
+
+        gl.glClearColor(1, 1, 1, 1);
+        gl.glEnable(GL.GL_TEXTURE_2D);
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
         gl.glGenTextures(textureNames.length, textures, 0);
-        
-        for(int i = 0; i < textureNames.length; i++){
+
+        for (int i = 0; i < textureNames.length; i++) {
             try {
-                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
+                texture[i] = TextureReader.readTexture(
+                        assetsFolderName + "//" + textureNames[i], true);
+
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
 
-//                mipmapsFromPNG(gl, new GLU(), texture[i]);
                 new GLU().gluBuild2DMipmaps(
-                    GL.GL_TEXTURE_2D,
-                    GL.GL_RGBA, // Internal Texel Format,
-                    texture[i].getWidth(), texture[i].getHeight(),
-                    GL.GL_RGBA, // External format from image,
-                    GL.GL_UNSIGNED_BYTE,
-                    texture[i].getPixels() // Imagedata
-                    );
-            } catch( IOException e ) {
-              System.out.println(e);
-              e.printStackTrace();
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA,
+                        texture[i].getWidth(), texture[i].getHeight(),
+                        GL.GL_RGBA,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture[i].getPixels()
+                );
+
+            } catch (IOException e) {
+                System.out.println(e);
             }
         }
-
-
     }
-    
+
     public void display(GLAutoDrawable gld) {
 
         GL gl = gld.getGL();
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);       //Clear The Screen And The Depth Buffer
-        gl.glLoadIdentity(); 
-        
-        DrawBackground(gl, 8);
-        handleKeyPress();
-        animationIndex = animationIndex % 4;
-        
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
 
+        DrawBackground(gl);
 
-        DrawSprite(gl, x, y, animationIndex, 3.1f);
-        //
+        frameDelay++;
 
+        // Detect movement
+        boolean moving =
+                isKeyPressed(KeyEvent.VK_LEFT) ||
+                        isKeyPressed(KeyEvent.VK_RIGHT);
 
+        handleKeyPress(moving);
 
+        boolean animate = frameDelay % 7 == 0;
 
+        if (animate) {
+            if (moving) {
+                walkIndex = (walkIndex + 1) % walkFrames;
+            } else {
+                idleIndex = (idleIndex + 1) % idleFrames;
+            }
+        }
+
+        // DRAW CHARACTER
+        if (moving) {
+            DrawSprite(gl, x, y, walkIndex, 2.4f, facingLeft);
+        } else {
+            DrawSprite(gl, x, y, 8 + idleIndex, 2.4f, facingLeft);
+        }
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
 
     }
 
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean b, boolean b1) {
+
     }
 
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
-    }
-    
-    public void DrawSprite(GL gl,int x, int y, int index, float scale){
+    // ------------------------------------------------------------
+    // Draw Sprite
+    // ------------------------------------------------------------
+    public void DrawSprite(GL gl, int x, int y, int index, float scale, boolean flip) {
         gl.glEnable(GL.GL_BLEND);
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);	// Turn Blending On
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);
 
         gl.glPushMatrix();
-            gl.glTranslated( x/(maxWidth/2.0) - 0.9, y/(maxHeight/2.0) - 0.9, 0);
-            gl.glScaled(0.1*scale, 0.1*scale, 1);
-            //System.out.println(x +" " + y);
-            gl.glBegin(GL.GL_QUADS);
-            // Front Face
-                gl.glTexCoord2f(0.0f, 0.0f);
-                gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 0.0f);
-                gl.glVertex3f(1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 1.0f);
-                gl.glVertex3f(1.0f, 1.0f, -1.0f);
-                gl.glTexCoord2f(0.0f, 1.0f);
-                gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-            gl.glEnd();
+        gl.glTranslated(x / (maxWidth / 2.0) - 0.9, y / (maxHeight / 2.0) - 0.9, 0);
+
+        if (flip)
+            gl.glScaled(-0.1 * scale, 0.1 * scale, 1);
+        else
+            gl.glScaled(0.1 * scale, 0.1 * scale, 1);
+
+        gl.glBegin(GL.GL_QUADS);
+        gl.glTexCoord2f(0, 0);
+        gl.glVertex3f(-1, -1, -1);
+        gl.glTexCoord2f(1, 0);
+        gl.glVertex3f(1, -1, -1);
+        gl.glTexCoord2f(1, 1);
+        gl.glVertex3f(1, 1, -1);
+        gl.glTexCoord2f(0, 1);
+        gl.glVertex3f(-1, 1, -1);
+        gl.glEnd();
+
         gl.glPopMatrix();
-        
         gl.glDisable(GL.GL_BLEND);
     }
-    
-    public void DrawBackground(GL gl, int idx){
-        gl.glEnable(GL.GL_BLEND);	
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[idx]);	// Turn Blending On
+
+    // ------------------------------------------------------------
+    // Draw Background (index 14)
+    // ------------------------------------------------------------
+    public void DrawBackground(GL gl) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[14]);
 
         gl.glPushMatrix();
-            gl.glBegin(GL.GL_QUADS);
-            // Front Face
-                gl.glTexCoord2f(0.0f, 0.0f);
-                gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 0.0f);
-                gl.glVertex3f(1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 1.0f);
-                gl.glVertex3f(1.0f, 1.0f, -1.0f);
-                gl.glTexCoord2f(0.0f, 1.0f);
-                gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-            gl.glEnd();
+        gl.glBegin(GL.GL_QUADS);
+        gl.glTexCoord2f(0, 0);
+        gl.glVertex3f(-1, -1, -1);
+        gl.glTexCoord2f(1, 0);
+        gl.glVertex3f(1, -1, -1);
+        gl.glTexCoord2f(1, 1);
+        gl.glVertex3f(1, 1, -1);
+        gl.glTexCoord2f(0, 1);
+        gl.glVertex3f(-1, 1, -1);
+        gl.glEnd();
         gl.glPopMatrix();
-        
+
         gl.glDisable(GL.GL_BLEND);
     }
-    
-    /*
-     * KeyListener
-     */    
 
-    public void handleKeyPress() {
+    // ------------------------------------------------------------
+    // Movement + Facing Direction
+    // ------------------------------------------------------------
+    public void handleKeyPress(boolean moving) {
 
         if (isKeyPressed(KeyEvent.VK_LEFT)) {
-            if (x > 0) {
-                x--;
-            }
-            animationIndex++;
+            facingLeft = true;
+            if (x > 0) x--;
         }
+
         if (isKeyPressed(KeyEvent.VK_RIGHT)) {
-            if (x < maxWidth-10) {
-                x++;
-            }
-            animationIndex++;
+            facingLeft = false;
+            if (x < maxWidth - 10) x++;
         }
-        if (isKeyPressed(KeyEvent.VK_DOWN)) {
-            if (y > 0) {
-//                y--;
-            }
-            animationIndex++;
-        }
+
+        // Optional Y movement
         if (isKeyPressed(KeyEvent.VK_UP)) {
-            if (y < maxHeight-10) {
-//                y++;
-            }
-            animationIndex++;
+            // if (y < maxHeight - 10) y++;
+        }
+
+        if (isKeyPressed(KeyEvent.VK_DOWN)) {
+            // if (y > 0) y--;
         }
     }
 
-    public BitSet keyBits = new BitSet(256);
- 
-    @Override 
-    public void keyPressed(final KeyEvent event) {
-        int keyCode = event.getKeyCode();
-        keyBits.set(keyCode);
-    } 
- 
-    @Override 
-    public void keyReleased(final KeyEvent event) {
-        int keyCode = event.getKeyCode();
-        keyBits.clear(keyCode);
-    } 
- 
-    @Override 
-    public void keyTyped(final KeyEvent event) {
-        // don't care 
-    } 
- 
-    public boolean isKeyPressed(final int keyCode) {
+    // ----------------- Keyboard Handling -----------------
+    BitSet keyBits = new BitSet(256);
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keyBits.set(e.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keyBits.clear(e.getKeyCode());
+    }
+
+    public boolean isKeyPressed(int keyCode) {
         return keyBits.get(keyCode);
     }
 }
